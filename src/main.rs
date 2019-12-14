b17e9760ecc9ef00fe871a7e8556157125b86877
Pierre Wacrenier <pierre@wacrenier.me>
2019-12-14 11:36:36 -0800

    Handle refresh frequencies

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,44 @@ const ROM_SIZE: usize = MEMORY_SIZE - 0x200;
 const WIDTH: usize = 64;
 const HEIGHT: usize = 32;
 const UPSCALE: usize= 10;
+const CPU_FREQUENCY: &'static str = "500";
+const IO_FREQUENCY: &'static str = "60";
+
+struct Counter {
+    slice: u64,
+    counter: time::Instant,
+}
+
+impl Counter {
+    pub fn new(frequency: u64) -> Self {
+        Self {
+            slice: time::Duration::from_millis(1000 / frequency).as_millis() as u64,
+            counter: time::Instant::now(),
+        }
+    }
+
+    pub fn from_str(frequency: &str) -> Self {
+        let int_value = frequency.to_string().parse::<u64>().unwrap();
+
+        Self::new(int_value)
+    }
+
+    pub fn burnt_duration(&self) -> i128 {
+        self.slice as i128 - self.counter.elapsed().as_millis() as i128
+    }
+
+    pub fn is_burnt(&self) -> bool {
+        self.burnt_duration() <= 0
+    }
+
+    pub fn reset(&mut self) {
+        self.counter = time::Instant::now()
+    }
+
+    pub fn duration(&self) -> time::Duration {
+        self.counter.elapsed()
+    }
+}
 
 pub fn main() {
     let matches = App::new(env!("CARGO_PKG_NAME"))
@@ -36,7 +74,22 @@ pub fn main() {
             .help("path to the rom file")
             .required(true)
             )
+        .arg(Arg::with_name("cpu_freq")
+            .short("c")
+            .long("cpu-freq")
+            .value_name("cpu_freq")
+            .help("Set the frequency of the CPU clock in Hz")
+            )
+        .arg(Arg::with_name("io_freq")
+            .short("i")
+            .long("io-freq")
+            .value_name("io_freq")
+            .help("Set the display and buzzer refresh rate in Hz")
+            )
         .get_matches();
+
+    let mut cpu_counter = Counter::from_str(matches.value_of("cpu_freq").unwrap_or(CPU_FREQUENCY));
+    let mut io_counter = Counter::from_str(matches.value_of("io_freq").unwrap_or(IO_FREQUENCY));
 
     let rom_path = matches.value_of("ROM").unwrap();
     let rom = load_rom(rom_path).unwrap();
@@ -50,16 +103,22 @@ pub fn main() {
 
     renderer.reset();
     while let Ok(state) = keyboard.poll() {
-        cpu.tick(&state);
-
-        renderer.render(cpu.get_vram());
-        if cpu.beeping() {
-            buzzer.play()
-        } else {
-            buzzer.pause()
+        if io_counter.is_burnt() {
+            renderer.render(cpu.get_vram());
+            if cpu.beeping() {
+                buzzer.play()
+            } else {
+                buzzer.pause()
+            }
+            io_counter.reset()
         }
 
-        // tmp 500hz
-        thread::sleep(time::Duration::from_millis(2));
+        if cpu_counter.is_burnt() {
+            cpu.tick(&state);
+            cpu_counter.reset()
+        } else {
+            // This can be safely assume for a 500hz CPU block and 60HZ display refresh rate.
+            thread::sleep(time::Duration::from_millis(cpu_counter.burnt_duration() as u64));
+        }
     }
 }
